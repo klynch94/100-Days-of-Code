66131079b5d21561174c6f6fc94ac2063aecff36
Kieran Lynch <lynch.kieran1@gmail.com>
2020-05-26 16:47:28 -0500

    new days

---
+++ Day4.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/javascript
+// Level: 8kyu
+
+// Instructions:
+// Get ASCII value of a character.
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+// Function:
+function getASCII(c){
+    var charCode = c.charCodeAt(0)
+    return charCode;
+  }
